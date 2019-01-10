728c0d8d2791937ce1579da101a2a20ae02ccdbb
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2019-01-10 11:59:27 -0600

    Simple script to see VINCI-specific lines.

---
+++ proportion.py
@@ -0,0 +1,13 @@
+files = ['Fobt.sql', 'HCC.sql', 'IDA.sql', 'Lung.sql']
+search_str = 'Singh'
+
+for f in files:
+    lines = open(f).read().splitlines()
+    numer = 0
+    denom = len(lines)
+    for L in lines:
+        if search_str in L:
+            numer += 1
+    proportion_str = str(round(numer / denom, 3))
+    print('%d / %d\tlines in %s\tcontain "%s", = %s.' % \
+              (numer, denom, f, search_str, proportion_str))
