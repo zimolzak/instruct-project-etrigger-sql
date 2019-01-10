ec94b8c1634b18a6e37d8514768639ed7b498db3
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2019-01-10 14:22:43 -0600

    Now works in Python 2 as well.

--- proportion.py
+++ proportion.py
@@ -8,6 +8,6 @@ for f in files:
     for L in lines:
         if search_str in L:
             numer += 1
-    proportion_str = str(round(numer / denom, 3))
+    proportion_str = str(round(numer / float(denom), 3))
     print('%d / %d\tlines in %s\tcontain "%s", = %s.' % \
               (numer, denom, f, search_str, proportion_str))
