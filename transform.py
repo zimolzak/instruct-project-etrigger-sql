b22c9c55d5bac4fa7e9f428cb2c8fab41006a640
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2019-09-05 10:02:03 -0500

    Lots of logic and string edit for Src case, seems working.

--- transform.py
+++ transform.py
@@ -7,6 +7,8 @@ fh = open(sys.argv[1])
 vinci = re.compile(re.escape('VINCI1'), re.IGNORECASE)
 ord_singh_src_all = re.compile("\[?ORD_Singh[^ -')]*Src[^ -')]*\]?", re.IGNORECASE)
 ord_singh_src_stop = re.compile("\[?ORD_Singh[^ -')]*Src\]?", re.IGNORECASE)
+ord_singh_src_backref = re.compile("\[?ORD_Singh[^ -')]*Src\]?\\.(\[?\w+\]?)", re.IGNORECASE)
+#                                    [ ORD_Singh.....].[Src ]   . [  a_b  ]
 ord_dflt = re.compile("\[?ORD_Singh[^ -')]*Dflt\]?", re.IGNORECASE)
 
 for L in fh.read().splitlines():
@@ -15,7 +17,12 @@ for L in fh.read().splitlines():
         comment = DEFAULT_COMMENT
     if ord_singh_src_all.search(L):
         comment = '    --alert'
-    L = ord_singh_src_stop.sub('[CDWWork]', L)
+        m = ord_singh_src_backref.search(L)
+        underscores = m.group(1)                                                # '[inpat_inpatientcptprocedure]'
+        dots = underscores.replace('_', '.', 1).replace('[','').replace(']','') # 'inpat.inpatientcptprocedure'
+        dot_brkt = '[' + dots.replace('.', '].[') + ']'
+        L = ord_singh_src_stop.sub('[CDWWork]', L)
+        L = L.replace(underscores, dot_brkt)
     L = vinci.sub('[CDWWork]', L)
     L = ord_dflt.sub('[MyDB].[MySchema]', L)
     print(L + comment)
