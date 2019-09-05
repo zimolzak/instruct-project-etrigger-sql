c3e078677cdb989799c6877b36d22d360c16f168
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2019-09-05 09:33:30 -0500

    Beginnings of fixing the ORD...Src tables.

--- transform.py
+++ transform.py
@@ -5,13 +5,17 @@ assert sys.version_info.major >= 3
 DEFAULT_COMMENT = '    --altered by Python'
 fh = open(sys.argv[1])
 vinci = re.compile(re.escape('VINCI1'), re.IGNORECASE)
-ord_singh = re.compile(re.escape('ORD_Singh'), re.IGNORECASE)
+ord_singh_src_all = re.compile("\[?ORD_Singh[^ -')]*Src[^ -')]*\]?", re.IGNORECASE)
+ord_singh_src_stop = re.compile("\[?ORD_Singh[^ -')]*Src\]?", re.IGNORECASE)
 ord_dflt = re.compile("\[?ORD_Singh[^ -')]*Dflt\]?", re.IGNORECASE)
 
 for L in fh.read().splitlines():
     comment = ''
     if vinci.search(L) or ord_dflt.search(L):
         comment = DEFAULT_COMMENT
-    L = vinci.sub('CDWWork', L)
+    if ord_singh_src_all.search(L):
+        comment = '    --alert'
+    L = ord_singh_src_stop.sub('[CDWWork]', L)
+    L = vinci.sub('[CDWWork]', L)
     L = ord_dflt.sub('[MyDB].[MySchema]', L)
     print(L + comment)
