60ef735972807a6aec33e7fd043a8d1596aa0a58
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-02-22 10:22:56 -0600

    Logic for OBJECT_ID and tempdb.dbo seems to be working.

--- advanced/transform_temptables.py
+++ advanced/transform_temptables.py
@@ -4,13 +4,17 @@ assert sys.version_info.major >= 3
 
 fh = open(sys.argv[1])
 mydb_backref = re.compile("\[MyDB\]\.\[MySchema\]\.\[?(\w+)\]?", re.IGNORECASE)
-#                              [MyDB].[MySchema].[asdfasdf]
+#                              [MyDB].[MySchema].[table_name]
 
 for L in fh.read().splitlines():
     comment = ''
     m = mydb_backref.search(L)
     if m:
-        comment = '    --altered (temp table)'
-        table_name = m.group(1)                                                # '[inpat_inpatientcptprocedure]'
-        L = mydb_backref.sub('#' + table_name, L)
+        table_name = m.group(1)
+        if 'OBJECT_ID' in L:
+            comment = ' --altered (object_id temp table)'
+            L = mydb_backref.sub('tempdb.dbo.#' + table_name, L)
+        else:
+            comment = ' --altered (temp table)'
+            L = mydb_backref.sub('#' + table_name, L)
     print(L + comment)
